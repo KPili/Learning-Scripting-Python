281672790f6e52c8000c4a2a4314ed89f9fcfd0c
Kurt <kurtpili3@gmail.com>
2021-07-05 19:34:09 -0500

    Changed the name of img variable to the pokemon it represents. Also added in a pikachu image variable.

--- show_image.py
+++ show_image.py
@@ -1,12 +1,14 @@
 # This Python file uses the foloowing encoding: utf-8
 from PIL import Image, ImageFilter
 
-img = Image.open('./Pokedex/bulbasaur.jpg')
+bulbasaur = Image.open('./Pokedex/bulbasaur.jpg')
+pikachu = Image.open('./Pokedex/pikachu.jpg')
 
 # Adding affects
-sharp_plant = img.filter(ImageFilter.SHARPEN)
+sharp_plant = bulbasaur.filter(ImageFilter.SHARPEN)
 turn_ninety_degrees = sharp_plant.rotate(90)
-resize = img.resize((100, 100))
+resize = bulbasaur.resize((100, 100))
+
 
 # Saving to folder
 sharp_plant.save("./altered_imgs/bulbaSORE.png", 'png')
