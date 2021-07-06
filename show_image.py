20b401250e54089eaa962eb99a05e8aca56830c7
Kurt <kurtpili3@gmail.com>
2021-07-05 19:27:18 -0500

    Created a new variable to hold the sharpened image, and then applied the rotate method to turn the image 90 degrees. Saved accordingly into the altered_imgs folder.

--- show_image.py
+++ show_image.py
@@ -4,5 +4,7 @@ from PIL import Image, ImageFilter
 img = Image.open('./Pokedex/bulbasaur.jpg')
 sharp_plant = img.filter(ImageFilter.SHARPEN)
 
+turn_ninety_degrees = sharp_plant.rotate(90)
+turn_ninety_degrees.save("./altered_imgs/bulbaSORE90.png", 'png')
 sharp_plant.save("./altered_imgs/bulbaSORE.png", 'png')
-sharp_plant.show()
+# sharp_plant.show()
