658697fdb3a8ea5356dde56993309a516e891ba6
Kurt <kurtpili3@gmail.com>
2021-07-05 19:37:14 -0500

    Added crop dimensions, created a crop_pikachu variable, saved this image in altered_imgs

--- show_image.py
+++ show_image.py
@@ -8,10 +8,12 @@ pikachu = Image.open('./Pokedex/pikachu.jpg')
 sharp_plant = bulbasaur.filter(ImageFilter.SHARPEN)
 turn_ninety_degrees = sharp_plant.rotate(90)
 resize = bulbasaur.resize((100, 100))
-
+crop_dimensions = (100, 100, 400, 400)
+crop_pikachu = pikachu.crop(crop_dimensions)
 
 # Saving to folder
 sharp_plant.save("./altered_imgs/bulbaSORE.png", 'png')
 turn_ninety_degrees.save("./altered_imgs/bulbaSORE90.png", 'png')
 resize.save("./altered_imgs/smallasour.png", 'png')
+crop_pikachu.save("./altered_imgs/crop.png", 'png')
 # sharp_plant.show()
